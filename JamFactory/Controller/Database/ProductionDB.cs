cb91ed6e0fa9e6e64d9376b1b03d83cf2813f97d
Sadri TBA <sadri_albaner@hotmail.com>
2015-03-03 13:32:09 +0100

    så peter

--- JamFactory/Controller/Database/ProductionDB.cs
+++ JamFactory/Controller/Database/ProductionDB.cs
@@ -7,65 +7,69 @@ using System.Text;
 using System.Threading.Tasks;
 using JamFactory.Model;
 
-namespace JamFactory.Controller.Database {
-    public class ProductionDB {
+namespace JamFactory.Controller.Database
+{
+    public class ProductionDB
+    {
         static string ConnectionString = "Server=ealdb1.eal.local;" + "Database=ejl20_db;" + "User Id=ejl20_usr;" + "Password=Baz1nga20;";
 
-      public static void CheckLogin(int PersonID, string Password)
-      {
-            try
+        public static void CheckLogin(int PersonID, string Password)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
             {
-                using (SqlConnection conn = new SqlConnection(ConnectionString))
+                try
                 {
-                     SqlCommand cmd = new SqlCommand("2_CheckLogin", conn);
-                     conn.Open();
-                     cmd.CommandType = CommandType.StoredProcedure;
-                     cmd.Parameters.Add(new SqlParameter("@PersonID", PersonID));
-                     cmd.Parameters.Add(new SqlParameter("@Password", Password));
-                    cmd.ExecuteNonQuery();
+                    SqlCommand cmd = new SqlCommand("2_CheckLogin", conn);
+                    conn.Open();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@PersonID", PersonID));
+                    cmd.Parameters.Add(new SqlParameter("@Password", Password));
+                    cmd.ExecuteNonQuery(); // ExecuteNonQuery is intended for UPDATE, INSERT and DELETE queries
+
+                }
+                catch (SqlException E)
+                {
+                    System.Windows.MessageBox.Show(E.Message);
+                }
+                finally
+                {
                     conn.Close();
+                    conn.Dispose();
                 }
             }
-            catch (Exception E) {
-                throw E;
-            }
-            finally {
-                    cmd.ExecuteNonQuery(); // ExecuteNonQuery is intended for UPDATE, INSERT and DELETE queries
- 
-            }
         }
-        public static List<Model.Task> GetTask(Model.Employee employee, DateTime starttime, DateTime endtime) {
+
+        public static List<Model.Task> GetTask(Model.Employee employee, DateTime starttime, DateTime endtime)
+        {
             List<Model.Task> _Tasks = new List<Model.Task>();
-            using (SqlConnection conn = new SqlConnection(ConnectionString)) {
-                try {
-                    
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                try
+                {
                     SqlCommand cmd = new SqlCommand("2_CheckLogin", conn);
                     conn.Open();
                     cmd.CommandType = CommandType.StoredProcedure;
                     cmd.Parameters.Add(new SqlParameter("@PersonID", employee.ID));
                     cmd.Parameters.Add(new SqlParameter("@StartTime", starttime));
                     cmd.Parameters.Add(new SqlParameter("@EndTime", endtime));
-                     SqlDataReader reader = cmd.ExecuteReader();
- 
-                    while (reader.Read()) {
-                        _Tasks.Add(new Model.Task(reader["Description"].ToString(), Convert.ToDateTime(reader["StartTime"].ToString()), Convert.ToDateTime(reader["EndTime"].ToString()), employee, new Model.Machine(reader["Name"].ToString(), int.Parse(reader["Capacity"].ToString()), int.Parse(reader["ScrapValue"].ToString()), double.Parse(reader["AcquisitionValue"].ToString()), Convert.ToDateTime(reader["LifeTime"].ToString()))));
-                    
-                }
-                    else
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
                     {
- 
-                catch (SqlException e) {
-                    System.Windows.MessageBox.Show(e.Message);
-                 }
-                finally {
- 
+                        _Tasks.Add(new Model.Task(reader["Description"].ToString(), Convert.ToDateTime(reader["StartTime"].ToString()), Convert.ToDateTime(reader["EndTime"].ToString()), employee, new Model.Machine(reader["Name"].ToString(), int.Parse(reader["Capacity"].ToString()), int.Parse(reader["ScrapValue"].ToString()), double.Parse(reader["AcquisitionValue"].ToString()), Convert.ToDateTime(reader["LifeTime"].ToString()))));
+                    }
+                }
+                catch (SqlException E)
+                {
+                    System.Windows.MessageBox.Show(E.Message);
+                }
+                finally
+                {
                     conn.Close();
-             conn.Dispose();
-          }
-        return _Tasks;
-
-         }
- 
-
+                    conn.Dispose();
+                }
+                return _Tasks;
+            }
+        }
     }
 }
